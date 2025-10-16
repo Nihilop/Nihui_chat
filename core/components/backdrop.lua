11c8600130511dda5452eaac388b02253b6cac53
Nihilo <nihilo@outlook.fr>
2025-10-16 22:10:11 +0200

    update clone

---
+++ core/components/backdrop.lua
@@ -0,0 +1,106 @@
+local _, ns = ...
+local E, C, D, L = ns.E, ns.C, ns.D, ns.L
+
+-- Lua
+local _G = getfenv(0)
+local t_insert = _G.table.insert
+
+-- Mine
+local backdrops = {}
+
+local backdrop_proto = {}
+do
+	function backdrop_proto:UpdateAlpha(a)
+		-- Keep castbar style dark background if it's a castbar backdrop
+		if self.isCastbarBackdrop then
+			self:SetBackdropColor(0.2, 0.2, 0.2, a)
+			self:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
+		-- Keep Nihui_uf border color if it's a tab backdrop
+		elseif self.isTabBackdrop then
+			self:SetBackdropColor(0, 0, 0, a)
+			self:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
+		else
+			self:SetBackdropColor(0, 0, 0, a)
+			self:SetBackdropBorderColor(0, 0, 0, a)
+		end
+	end
+
+	function backdrop_proto:UpdateOffsets(xOffset, yOffset)
+		self:SetPoint("TOPLEFT", xOffset or 0, -(yOffset or 0))
+		self:SetPoint("BOTTOMRIGHT", -(xOffset or 0), yOffset or 0)
+	end
+end
+
+function E:CreateBackdrop(parent, alpha,  xOffset, yOffset)
+	local backdrop = Mixin(CreateFrame("Frame", nil, parent, "BackdropTemplate"), backdrop_proto)
+	backdrop:SetFrameLevel(parent:GetFrameLevel() - 1)
+	backdrop:SetPoint("TOPLEFT", xOffset or 0, -(yOffset or 0))
+	backdrop:SetPoint("BOTTOMRIGHT", -(xOffset or 0), yOffset or 0)
+	backdrop:SetBackdrop({
+		bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+		edgeFile = "Interface\\AddOns\\ls_Glass\\assets\\border",
+		tile = true,
+		tileEdge = true,
+		tileSize = 8,
+		edgeSize = 8,
+		-- insets = {left = 4, right = 4, top = 4, bottom = 4},
+	})
+
+	-- the way Blizz position it creates really weird gaps, so fix it
+	backdrop.Center:ClearAllPoints()
+	backdrop.Center:SetPoint("TOPLEFT", backdrop.TopLeftCorner, "BOTTOMRIGHT", 0, 0)
+	backdrop.Center:SetPoint("BOTTOMRIGHT", backdrop.BottomRightCorner, "TOPLEFT", 0, 0)
+
+	backdrop:SetBackdropColor(0, 0, 0, alpha)
+	backdrop:SetBackdropBorderColor(0, 0, 0, alpha)
+
+	t_insert(backdrops, backdrop)
+
+	return backdrop
+end
+
+-- Create backdrop with Nihui_uf style border
+function E:CreateTabBackdrop(parent, alpha, xOffset, yOffset)
+	local backdrop = Mixin(CreateFrame("Frame", nil, parent, "BackdropTemplate"), backdrop_proto)
+	backdrop.isTabBackdrop = true
+	backdrop:SetFrameLevel(parent:GetFrameLevel() - 1)
+	backdrop:SetPoint("TOPLEFT", -12, 12)
+	backdrop:SetPoint("BOTTOMRIGHT", 12, -12)
+	backdrop:SetBackdrop({
+		bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+		edgeFile = "Interface\\AddOns\\Nihui_chat\\textures\\MirroredFrameSingleUF",
+		tile = false,
+		edgeSize = 16,
+		insets = {left = 12, right = 12, top = 12, bottom = 12},
+	})
+
+	backdrop:SetBackdropColor(0, 0, 0, alpha)
+	backdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1) -- Nihui_uf gray border
+
+	t_insert(backdrops, backdrop)
+
+	return backdrop
+end
+
+-- Create backdrop with Nihui_cb castbar style border
+function E:CreateCastbarBackdrop(parent, alpha)
+	local backdrop = Mixin(CreateFrame("Frame", nil, parent, "BackdropTemplate"), backdrop_proto)
+	backdrop.isCastbarBackdrop = true
+	backdrop:SetFrameLevel(parent:GetFrameLevel() - 1)
+	backdrop:SetPoint("TOPLEFT", -12, 12)
+	backdrop:SetPoint("BOTTOMRIGHT", 12, -12)
+	backdrop:SetBackdrop({
+		bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+		edgeFile = "Interface\\AddOns\\Nihui_chat\\textures\\MirroredFrameSingle2",
+		tile = false,
+		edgeSize = 16,
+		insets = {left = 1, right = 1, top = 1, bottom = 1},
+	})
+
+	backdrop:SetBackdropColor(0.2, 0.2, 0.2, alpha)
+	backdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1) -- Nihui_cb gray border
+
+	t_insert(backdrops, backdrop)
+
+	return backdrop
+end
---
+++ core/components/editbox.lua
@@ -0,0 +1,120 @@
+local _, ns = ...
+local E, C, D, L = ns.E, ns.C, ns.D, ns.L
+
+-- Lua
+local _G = getfenv(0)
+local next = _G.next
+
+-- Mine
+local handledEditBoxes = {}
+
+local EDIT_BOX_TEXTURES = {
+	"Left",
+	"Mid",
+	"Right",
+
+	"FocusLeft",
+	"FocusMid",
+	"FocusRight",
+}
+
+function E:HandleEditBox(frame)
+	if not handledEditBoxes[frame] then
+		-- Auto-hide functionality
+		if C.db.profile.edit.autohide then
+			-- Hide by default
+			frame:SetAlpha(0)
+			frame:EnableMouse(false)
+
+			-- Show on focus
+			frame:HookScript("OnEditFocusGained", function(self)
+				if C.db.profile.edit.autohide then
+					self:SetAlpha(1)
+					self:EnableMouse(true)
+				end
+			end)
+
+			-- Hide on focus lost
+			frame:HookScript("OnEditFocusLost", function(self)
+				if C.db.profile.edit.autohide then
+					self:SetAlpha(0)
+					self:EnableMouse(false)
+				end
+			end)
+		end
+
+		handledEditBoxes[frame] = true
+	end
+
+	-- Always recreate backdrop to apply new style
+	if frame.Backdrop then
+		frame.Backdrop:Hide()
+		frame.Backdrop = nil
+	end
+	frame.Backdrop = E:CreateCastbarBackdrop(frame, C.db.profile.edit.alpha)
+
+	for _, texture in next, EDIT_BOX_TEXTURES do
+		_G[frame:GetName() .. texture]:SetTexture(0)
+	end
+
+	frame:SetMultiLine(C.db.profile.edit.multiline)
+	frame:SetAltArrowKeyMode(C.db.profile.edit.alt)
+	frame:SetHeight(48)
+	frame:ClearAllPoints()
+
+	if C.db.profile.edit.position == "top" then
+		frame:SetPoint("TOPLEFT", frame.chatFrame, "TOPLEFT", 0, C.db.profile.edit.offset)
+		frame:SetPoint("TOPRIGHT", frame.chatFrame, "TOPRIGHT", 0, C.db.profile.edit.offset)
+	else
+		frame:SetPoint("BOTTOMLEFT", frame.chatFrame, "BOTTOMLEFT", 0, -C.db.profile.edit.offset)
+		frame:SetPoint("BOTTOMRIGHT", frame.chatFrame, "BOTTOMRIGHT", 0, -C.db.profile.edit.offset)
+	end
+
+	frame:SetFontObject("LSGlassEditBoxFont")
+	frame.header:SetFontObject("LSGlassEditBoxFont")
+	frame.headerSuffix:SetFontObject("LSGlassEditBoxFont")
+	frame.NewcomerHint:SetFontObject("LSGlassEditBoxFont")
+	frame.prompt:SetFontObject("LSGlassEditBoxFont")
+end
+
+function E:UpdateEditBoxPosition()
+	local isOnTop = C.db.profile.edit.position == "top"
+	local offset = C.db.profile.edit.offset
+
+	for editBox in next, handledEditBoxes do
+		editBox:ClearAllPoints()
+
+		if isOnTop then
+			editBox:SetPoint("TOPLEFT", editBox.chatFrame, "TOPLEFT", 0, offset)
+			editBox:SetPoint("TOPRIGHT", editBox.chatFrame, "TOPRIGHT", 0, offset)
+		else
+			editBox:SetPoint("BOTTOMLEFT", editBox.chatFrame, "BOTTOMLEFT", 0, -offset)
+			editBox:SetPoint("BOTTOMRIGHT", editBox.chatFrame, "BOTTOMRIGHT", 0, -offset)
+		end
+	end
+end
+
+function E:UpdateEditBoxMultiLine()
+	local isMultiline = C.db.profile.edit.multiline
+
+	for editBox in next, handledEditBoxes do
+		editBox:SetMultiLine(isMultiline)
+		editBox:SetHeight(48)
+	end
+end
+
+function E:UpdateEditBoxAltArrowKeyMode()
+	local altMode = C.db.profile.edit.alt
+
+	for editBox in next, handledEditBoxes do
+		editBox:SetAltArrowKeyMode(altMode)
+	end
+end
+
+function E:UpdateEditBoxAlpha()
+	local alpha = C.db.profile.edit.alpha
+
+	for editBox in next, handledEditBoxes do
+		editBox.Backdrop:UpdateAlpha(alpha)
+	end
+end
